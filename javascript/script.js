fd4a18f1f62da9e3e1ea89262ec9f5fa6b4af6f8
Natécia Costa <natecia_ilha@hotmail.com>
2026-01-16 14:51:20 -0300

    novo projeto

---
+++ javascript/script.js
@@ -0,0 +1,31 @@
+function contar() {
+    var num1 = document.getElementById('num1')
+    var num2 = document.getElementById('num2')
+    var passo = document.getElementById('pass')
+    var res = document.getElementById('res')
+
+    if (num1.value.length == 0 || num2.value.length == 0 || passo.value.length == 0) {
+        res.innerHTML = 'Impossível contar!'      
+    } else {
+        res.innerHTML = 'Contando: <br>'
+        var i = Number(num1.value)
+        var f = Number(num2.value)
+        var p = Number(passo.value)
+        if (p <= 0) {
+            alert('Passo inválido! Considerando PASSO 1')
+            p = 1
+        }
+        if (i < f) {
+            // Contagem crescente
+            for (var c = i; c <= f; c += p) {
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+        } else {
+            //Contagem decrescente
+            for (var c = i; c >= f; c -= p) {
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+        }        
+        res.innerHTML += `\u{1F3C1}`
+    }
+}
